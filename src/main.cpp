d96f1dbc78ac856f90956ad8b099504720e5b821
ReallyNewb <bradenlebron@icloud.com>
2026-01-03 19:22:01 -0600

    BUGFIX: Mod doesn't work on windows

--- src/main.cpp
+++ src/main.cpp
@@ -7,26 +7,6 @@
 using namespace geode::prelude;
 
 $execute {
-    if (sobriety::utils::isWine()) {
-        FileExplorer::get()->setup();
-        Console::get()->setup();
-    }
-    else {
-        (void) Mod::get()->uninstall();
-    }
+    FileExplorer::get()->setup();
+    Console::get()->setup();
 }
-
-class $modify(MenuLayer) {
-
-    bool init() {
-        if (!MenuLayer::init()) return false;
-
-        queueInMainThread([] {
-            if (!sobriety::utils::isWine()) {
-                createQuickPopup("Windows User Detected!", "Sobriety only works on <cg>Linux</c> systems and will do nothing on <cb>Windows</c>.\nIt has been <cr>uninstalled</c>.", "OK", nullptr, nullptr);
-            }
-        });
-
-        return true;
-    }
-};
